b822036a69cac7357fee06ee7fd82fbc80cbc7fa
Petr Tomasek <PeTomasek@csas.cz>
2024-12-06 14:58:26 +0100

    Initial Commit

---
+++ NativeTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NativeTest
+//
+//  Created by Petr Tomášek on 06.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NativeTest/NativeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeTestApp.swift
+//  NativeTest
+//
+//  Created by Petr Tomášek on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct NativeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
